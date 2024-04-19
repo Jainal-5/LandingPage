79e1c685a51786ac97ce2b1ef4ba600c30e7c649
Jainal-5 <asbijay@gmail.com>
2024-04-19 23:18:33 +0800

    Commit 2

--- mySite/settings.py
+++ mySite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0fezk!04bkpimlihpz2lsoi#x!hq&-10la=e5b4+96&^j%z&41
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
